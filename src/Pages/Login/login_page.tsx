af9d4b614de29b88c423a61e65a0566035086438
elfahim <amalou.free99@gmail.com>
2022-07-09 17:10:10 +0100

    start the login page

---
+++ src/Pages/Login/login_page.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const LoginPage= () => {
+    return (
+        <div> Login Page
+             <form>
+                <label> Username :</label>
+                <input> </input>
+                <label> Password :</label>
+                <input> </input>
+             </form>
+        </div>
+        
+    )
+        
+    
+}
+export default LoginPage
